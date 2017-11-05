3b685f5640288afea44a1f4a1ac82fe23aa8311a
Ejaz Ansari (Ejaz47) <ejjuak47@gmail.com>
2017-11-05 15:37:26 +0530

    url rewrite and bd erro on local solved

--- include/Config.php
+++ include/Config.php
@@ -2,10 +2,15 @@
 
 
 /** Database config */
-//define('DB_USERNAME', 'root');  
-//define('DB_PASSWORD', '');  
-define('DB_USERNAME', 'sqyard_2017');  
-define('DB_PASSWORD', 'Sqyard@!2017');  
+if($_SERVER['HTTP_HOST'] == 'localhost' || $_SERVER['HTTP_HOST'] == '127.0.0.1'){
+	define('DB_USERNAME', 'root');  
+	define('DB_PASSWORD', '');  
+}else
+{
+	define('DB_USERNAME', 'sqyard_2017');  
+	define('DB_PASSWORD', 'Sqyard@!2017');  
+}
+
 define('DB_HOST', 'localhost');  
 define('DB_NAME', 'sqyard_2017');
 
